393dbd35409b1a79b04abcdd141bf7f34629fa52
Rodrigogarciactg <100664773+Rodrigogarciactg@users.noreply.github.com>
2022-05-07 19:33:30 -0700

    MERN PT Jokes API
    POST, PUT, DELETE all work

--- server/controllers/jokes.controller.js
+++ server/controllers/jokes.controller.js
@@ -10,13 +10,9 @@ module.exports.findAllJokes = (req, res) => {
     });
 };
 module.exports.findOneSingleJoke = (req, res) => {
-  Joke.fineOne({ _id: req.params.id })
-    .then((oneSingleJoke) => {
-      res.json({ Joke: oneSingleJoke });
-    })
-    .catch((err) => {
-      res.json({ message: "Something went wrong", error: err });
-    });
+  Joke.findOne({ _id: req.params.id })
+    .then((joke) => res.json(joke))
+    .catch((err) => console.log(err));
 };
 module.exports.createNewJoke = (req, res) => {
   Joke.create(req.body)
@@ -28,23 +24,16 @@ module.exports.createNewJoke = (req, res) => {
     });
 };
 module.exports.updateExistingJoke = (req, res) => {
-  Joke.findOneAndUpdate({ _id: req.params.id }, req.body, {
+  Joke.findOneAndUpdate({ _id: req.params._id }, req.body, {
     new: true,
     runValidators: true,
   })
-    .then((updatedJoke) => {
-      res.json({ Joke: updatedJoke });
-    })
-    .catch((err) => {
-      res.json({ message: "something went wrong", error: err });
-    });
+    .then((updatedJoke) => res.json(updatedJoke))
+    .catch((err) => console.log(err));
 };
+
 module.exports.deleteAnExistingJoke = (req, res) => {
-  Joke.deleteOne({ _id: req.params.id })
-    .then((result) => {
-      res.json({ result: result });
-    })
-    .catch((err) => {
-      res.json({ message: "Something went wrong", error: err });
-    });
+  Joke.deleteOne({ _id: req.params._id })
+    .then((result) => res.json(result))
+    .catch((err) => console.log(err));
 };
--- server/routes/joke.routes.js
+++ server/routes/joke.routes.js
@@ -3,7 +3,7 @@ const JokeController = require("../controllers/jokes.controller");
 module.exports = (app) => {
   app.get("/api/jokes", JokeController.findAllJokes);
   app.post("/api/jokes", JokeController.createNewJoke);
-  app.get("/api/jokes/:_id", JokeController.findOneSingleJoke);
+  app.get("/api/jokes/:id", JokeController.findOneSingleJoke);
   app.put("/api/jokes/:_id", JokeController.updateExistingJoke);
   app.delete("/api/jokes/:_id", JokeController.deleteAnExistingJoke);
 };
